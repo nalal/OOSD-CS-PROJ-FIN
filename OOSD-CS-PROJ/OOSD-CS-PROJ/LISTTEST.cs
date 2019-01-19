604102531abf17982d7ebe1c9b0aa7212e08a7a6
Unknown <noahgman11@gmail.com>
2019-01-19 12:28:58 -0700

    Added a testing form

---
+++ OOSD-CS-PROJ/OOSD-CS-PROJ/LISTTEST.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOSD_CS_PROJ
+{
+    public partial class LISTTEST : Form
+    {
+        public LISTTEST()
+        {
+            InitializeComponent();
+            listBox1.DataSource = Packages.PackageID;
+        }
+    }
+}
