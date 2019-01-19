f07d28456f6d3992cb08d86977608bb153dddcf9
Unknown <noahgman11@gmail.com>
2019-01-19 08:04:41 -0700

    added error calls and DBConnection string

---
+++ OOSD-CS-PROJ/OOSD-CS-PROJ/DBCall.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOSD_CS_PROJ
+{
+    class DBCall
+    {
+    }
+}
---
+++ OOSD-CS-PROJ/OOSD-CS-PROJ/Err.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOSD_CS_PROJ
+{
+    class Err
+    {
+        //check if string is null
+        public static bool IsNonNullString(string s)
+        {
+            bool res = true;
+            if(s == null || s == "")
+            {
+                res = false;
+            }
+            return res;
+        }
+        //check if list is null
+        public static bool IsNonNullList(List<string> l)
+        {
+            bool res = true;
+            if (l.Count == 0)
+            {
+                res = false;
+            }
+            return res;
+        }
+        //check if string array is null
+        public static bool IsNonNullStringArray(string[] l)
+        {
+            bool res = true;
+            if (l.Count() == 0)
+            {
+                res = false;
+            }
+            return res;
+        }
+    }
+}
