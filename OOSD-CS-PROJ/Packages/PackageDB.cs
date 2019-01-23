9084f00555e5bb9f03d4e276c7962c08e09bb78d
Helen Lin <491198@ACDM.DS.SAIT.CA>
2019-01-23 13:47:18 -0700

    Updated Package.DB

--- OOSD-CS-PROJ/Packages/PackageDB.cs
+++ OOSD-CS-PROJ/Packages/PackageDB.cs
@@ -9,17 +9,21 @@ namespace OOSD_CS_PROJ
 {//Author: Helen Lin 
     public class PackageDB
     {
+        public static SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+        public static void InitSQL()
+        {
+            builder.DataSource = "localhost";
+            builder.UserID = "sa";
+            builder.Password = "PASSWORD";
+            builder.InitialCatalog = "TravelExperts";
+        }
         //this will allow user to add a new package to list
         public static int AddNewPackage(Package newPackage)
             //returns the auto-generated ID of the new Package
         {
             //PackageID will be auto incremented once created
             int PackageId;
-            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
-            builder.DataSource = "10.163.37.67";   // update me
-            builder.UserID = "sa";              // update me
-            builder.Password = "N1o2a3h4!"; // update me
-            builder.InitialCatalog = "TravelExperts";
+            
             using (SqlConnection conn = new SqlConnection(builder.ConnectionString))
             {
                 string insertStatement = "INSERT INTO Packages(PkgName, PkgStartDate, PkgEndDate, PkgDesc, PkgBasePrice, PkgAgencyCommission)" + "VALUES(@PkgName, @PkgStartDate, @PkgEndDate, @PkgDesc, @PkgBasePrice, @PkgAgencyCommission)";
