b4b6869b13e3bee1c97f6c721ffaeb90fe7b1089
mchadds <cdundassmith@gmail.com>
2019-01-30 11:14:19 -0700

    Beginning of ASP added
    Initial ASP file added to the project for Section five of the project

---
+++ OOSD-CS-PROJ/TravelExpertsASP/TravelExpertsASP/App_Code/TravelExperts1DB.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace TravelExpertsASP.App_Code
+{
+    public class TravelExperts1DB
+    {
+        // connecting to the DB
+         SqlConnection conn  = new SqlConnection("TravelExpertsConnectionString connectionString=Server= tcp:oosdgroup2.database.windows.net,1433; Initial Catalog = TravelExperts1; Persist Security Info=False;User ID = mchadds; Password=oOSD1234.;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout = 30;");
+    }
+}
